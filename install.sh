dd6eab641201e360adbf05c524cf2aae00c3d6da
otarza <otar.zakalashvili@gmail.com>
2020-05-13 14:12:55 +0400

    Adding development tools installation.

---
+++ install.sh
@@ -0,0 +1,18 @@
+# PHP
+sudo apt-get install php7.3-cli php7.3-curl
+
+
+#Platform SH CLI
+curl -sS https://platform.sh/cli/installer | php
+
+# ZSH
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/rupa/z.git
+
